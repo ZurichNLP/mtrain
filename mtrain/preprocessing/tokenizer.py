2a35c4de105e5dbc0d8247cc8c2ffe7e06391bbd
Samuel Läubli <laeubli@ifi.uzh.ch>
2016-08-11 18:50:50 +0200

    make tokenizer process segments instead of entire files

--- mtrain/preprocessing/tokenizer.py
+++ mtrain/preprocessing/tokenizer.py
@@ -1,37 +1,29 @@
 #!/usr/bin/env python3
 
 from mtrain.constants import *
+from mtrain.preprocessing.external import ExternalProcessor
 from mtrain.commander import run, run_parallel
 
 '''
 Tokenizes files using the default Moses tokenizer.
 '''
 
-def _get_tokenizer_command(filepath_origin, filepath_dest, lang_code):
-    tokenizer_args = ["-l", lang_code, "<", filepath_origin, ">", filepath_dest]
-    return " ".join([tokenizer_args])
+class Tokenizer(object):
+    '''
+    Creates a tokenizer which tokenizes sentences on-the-fly, i.e., allowing
+    interaction with a Moses tokenizer process kept in memory.
+    '''
 
-def tokenize(filepath_origin, filepath_dest, lang_code):
-    '''
-    Tokenizes the file located at @filepath_origin using the Moses tokenizer
-    for the language @lang_code. The tokenized output is stored at @param
-    filepath_dest.
-    '''
-    run(
-        _get_tokenizer_command(filepath_origin, filepath_dest, lang_code),
-        "Tokenizing %s" % filepath_origin
-    )
+    def __init__(self, lang_code):
+        self._processor = ExternalProcessor(
+            command=MOSES_TOKENIZER + "-b -X -a -l %s" % lang_code
+        )
 
-def tokenize_parallel(arguments):
-    '''
-    Tokenizes multiple files in parallel and waits untill all processes have
-    completed.
+    def __del__(self):
+        del self._processor
 
-    @param arguments a list of (filepath_origin, filepath_dest, lang_code)
-        tripples, one per file to be tokenized.
-    '''
-    run_parallel(
-        [_get_tokenizer_command(*a) for a in arguments],
-        "Tokenizing %s files in parallel: %s" %
-            (len(arguments), ", ".join([a[0] for a in arguments]))
-    )
+    def tokenize(self, segment):
+        '''
+        Tokenizes a single segment.
+        '''
+        return self._processor.process(segment).split(" ")
