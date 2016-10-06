59f4ca5c216e2377c68b4d94ce6e15ff207f9c2d
Mathias Müller <mathias.mueller@uzh.ch>
2016-10-06 11:39:01 +0200

    move engine to its own class

---
+++ mtrain/engine.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+import logging
+from mtrain.constants import *
+from mtrain.preprocessing.external import ExternalProcessor
+
+'''
+Translates preprocessed segments.
+'''
+
+class Engine(object):
+    '''
+    Starts a translation engine process and keep it running.
+    '''
+    def __init__(self, path_moses_ini, report_alignment=False, report_segmentation=False, allow_forced_translation=False):
+        '''
+        @param path_moses_ini path to Moses configuration file
+        @param report_alignment whether Moses should report word alignments
+        @param report_segmentation whether Moses should report how the translation
+            is made up of phrases
+        @param allow_forced_translation whether Moses should expect forced translations
+            as XML content
+        '''
+        self._path_moses_ini = path_moses_ini
+        self._report_alignment = report_alignment
+        self._report_segmentation = report_segmentation
+        self._allow_forced_translation = allow_forced_translation
+
+        arguments = [
+            '-f %s' % self._path_moses_ini,
+            '-minphr-memory', # compact phrase table
+            '-minlexr-memory', # compact reordering table
+            '-v 0', # as quiet as possible
+        ]
+        if self._report_alignment:
+            arguments.append('-print-alignment-info')
+        if self._report_segmentation:
+            arguments.append('-report-segmentation')
+        if self._allow_forced_translation:
+            arguments.append('-xml-input exclusive')        
+        
+        self._processor = ExternalProcessor(
+            command=" ".join([MOSES] + arguments),
+            stream_stderr=True
+        )
+
+    def close(self):
+        del self._processor
+
+    def translate_segment(self, segment):
+        '''
+        Translates a single input segment, @param segment.
+        @return a TranslatedSegment object with a translation and,
+        optionally, alignments and/or segmentation info
+        '''
+        translation = self._processor.process(segment)
+        
+        if self._report_alignment:
+            alignment = []
+            parts = translation.split('|||')
+            translation = parts[0].strip() # update translation to remove segmentation info
+            alignment = parts[1].strip().split(" ")
+        if self._report_segmentation:
+            tokens = []
+            segmentation = []
+            for string in translation.split(" "):
+                if '|' in string:
+                    segmentation.append(string)
+                else:
+                    tokens.append(string)
+            translation = " ".join(tokens) # update translation to only contain actual tokens
+        
+        return TranslatedSegment(
+            translated_segment=translation,
+            alignment=alignment if self._report_alignment else None,
+            segmentation=segmentation if self._report_segmentation else None
+        )
+        
+class TranslatedSegment(object):
+    '''
+    Models a single translated segment together with its word alignments and
+    phrase segmentation.
+    '''
+    def __init__(self, translated_segment, alignment=None, segmentation=None):
+        self._segment = translated_segment
+        self._alignment = alignment
+        self._segmentation = segmentation
--- mtrain/preprocessing/external.py
+++ mtrain/preprocessing/external.py
@@ -3,22 +3,36 @@
 '''
 Thread-safe wrapper for interaction with an external I/O shell script. Inspired
 by https://github.com/casmacat/moses-mt-server/blob/master/python_server/server.py
+
+Also inspired by eyalarubas.com/python-subproc-nonblock.html
 '''
 
 import threading
 import logging
 from subprocess import Popen, PIPE
+from queue import Queue, Empty
+
+from mtrain import commander
 
 class ExternalProcessor(object):
     '''
     Thread-safe wrapper for interaction with an external I/O shell script
     '''
 
-    def __init__(self, command):
+    def __init__(self, command, stream_stderr=False):
         self.command = command
+        self._stream_stderr = stream_stderr
         logging.debug("Executing %s", self.command)
-        self._process = Popen(self.command, shell=True, stdin=PIPE, stdout=PIPE)
+        self._process = Popen(
+            self.command,
+            shell=True,
+            stdin=PIPE,
+            stdout=PIPE,
+            stderr=PIPE if self._stream_stderr else None
+        )
         self._lock = threading.Lock()
+        if self._stream_stderr:
+            self._nbsr = _NonBlockingStreamReader(self._process.stderr)
 
     def close(self):
         '''
@@ -33,8 +47,53 @@ class ExternalProcessor(object):
         '''
         line = line.strip() + "\n"
         line = line.encode('utf-8')
+
         with self._lock:
             self._process.stdin.write(line)
             self._process.stdin.flush()
             result = self._process.stdout.readline()
+            # attempt reading from STDERR
+            if self._stream_stderr:
+                # grace period of 0.1 seconds for external process to return STDERR?
+                errors = self._nbsr.readline().decode()
+                if errors and commander._is_relevant_for_log(errors):
+                    logging.info(errors.strip())
         return result.decode().strip()
+
+class _NonBlockingStreamReader:
+    '''
+    Reads from stream without blocking, even if nothing can be read
+    '''
+    def __init__(self, stream):
+        '''
+        @param stream the stream to read from, usually a process' STDOUT or STDERR
+        '''
+
+        self._stream = stream
+        self._queue = Queue()
+
+        def _populateQueue(stream, queue):
+            '''
+            Collects lines from '@param stream and puts them in @param queue.
+            '''
+
+            while True:
+                line = stream.readline()
+                if line:
+                    queue.put(line)
+                else:
+                    raise _UnexpectedEndOfStream
+
+        self._thread = threading.Thread(target = _populateQueue,
+                args = (self._stream, self._queue))
+        self._thread.daemon = True
+        self._thread.start() # start collecting lines from the stream
+
+    def readline(self, timeout = None):
+        try:
+            return self._queue.get(block = timeout is not None,
+                    timeout = timeout)
+        except Empty:
+            return None
+
+class _UnexpectedEndOfStream(Exception): pass
