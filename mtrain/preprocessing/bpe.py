2731904271df5a8c886f836db06d481cec31c078
Beat Horat <horat@ifi.uzh.ch>
2017-11-18 14:15:37 +0100

    Fixed error in applying model, implemented buiding of dictionary

--- mtrain/preprocessing/bpe.py
+++ mtrain/preprocessing/bpe.py
@@ -13,7 +13,7 @@ class Encoder(object):
     ###BH text working with truecased only, as in wmt
     '''
 
-    def __init__(self, corpus_train_tc, corpus_eval_tc, bpe_model_path, bpe_operations, src_lang, trg_lang, tuning):
+    def __init__(self, corpus_train_tc, corpus_eval_tc, bpe_model_path, bpe_operations, src_lang, trg_lang, evaluation):
         '''
         ###BH todo all @params
         '''
@@ -24,19 +24,21 @@ class Encoder(object):
         self._bpe_operations=bpe_operations
         self._src_lang=src_lang
         self._trg_lang=trg_lang
-        self._tuning=tuning
+        self._evaluation=evaluation
 
 
     def learn_bpe_model(self):
         '''
-        ###BH todo text
+        Learn bpe model using truecased training corpus.
+        Stores the bpe model in the basepath's subfolder 'engine/bpe/', model name SRC-TRG.bpe.
         '''
+
         commander.run(
             'cat {corpus}.{src} {corpus}.{trg} | {script} --s {bpe_ops} > {bpe_model}/{src}-{trg}.bpe'.format(
-                corpus=self._corpus_train_tc,
+                corpus=self._corpus_train_tc,   # path of the truecased training corpus, no language ending
                 script=SUBWORD_NMT_LEARN,
                 bpe_ops=self._bpe_operations,
-                bpe_model=self._bpe_model_path,
+                bpe_model=self._bpe_model_path, # only bpe_model path, not file
                 src=self._src_lang,
                 trg=self._trg_lang
             ),
@@ -45,42 +47,43 @@ class Encoder(object):
 
     def apply_bpe_model(self):
         '''
-        ###BH todo text
-        language model built in learn_bpe_model() used to process:
-            'train.truecased.SRC'
-            'train.truecased.TRG'
-            'eval.truecased.SRC' (if existing)
-            'eval.truecased.TRG' (if existing)
+        Apply bpe model on truecased training corpus (and if present, truecased evaluation corpus).
+        Creates the files in the basepath's subfolder 'corpus', file names such as train.truecased.bpe.SRC, train.truecased.bpe.TRG
+        (and eval.truecased.bpe.SRC, eval.truecased.bpe.TRG if applicable).
         '''
 
         def command(current_corpus, current_lang):
             return '{script} -c {bpe_model}/{src}-{trg}.bpe < {corpus}.{lang} > {corpus}.bpe.{lang}'.format(
             script=SUBWORD_NMT_APPLY,
-            bpe_model=self._bpe_model_path,
-            src=self._src_lang,
-            trg=self._trg_lang,
-            corpus=current_corpus,
-            lang=current_lang
+            bpe_model=self._bpe_model_path,     # only bpe_model path, not file
+            src=self._src_lang,                 # for getting correct bpe_model file name
+            trg=self._trg_lang,                 # for getting correct bpe_model file name
+            corpus=current_corpus,              # either truecased training or evaluation corpus, depending on commander
+            lang=current_lang                   # either source or target language, depending on commander
             )
 
         commands = [
             command(self._corpus_train_tc, self._src_lang),
             command(self._corpus_train_tc, self._trg_lang),
         ]
-        if self._tuning:
+        if self._evaluation:
             commands.append(command(self._corpus_eval_tc, self._src_lang))
             commands.append(command(self._corpus_eval_tc, self._trg_lang))
         commander.run_parallel(commands, "Applying BPE model")
 
     def build_bpe_dictionary(self):
         '''
-        ###BH todo text
+        Build bpe dictionary (JSON files) for truecased training corpus.
+        Note that the JSON files such as train.truecased.bpe.SRC.json and train.truecased.bpe.TRG.json
+        are automatically stored at the location of the input files, which is the basepath's subfolder 'corpus'.
         '''
-        # processed using commander (no external processing)
 
-        ###BH implement using constant NEMATUS_BUILD_DICT
-
-
-    def close(self):
-    ###BH ckeck if used, else delete
-        del self._processor
+        commander.run(
+            '{script} {corpus}.bpe.{src} {corpus}.bpe.{trg}'.format(
+                script=NEMATUS_BUILD_DICT,
+                corpus=self._corpus_train_tc,   # path of the truecased training corpus, no language ending
+                src=self._src_lang,
+                trg=self._trg_lang,
+            ),
+            "Building network dictionary from BPE model"
+        )
