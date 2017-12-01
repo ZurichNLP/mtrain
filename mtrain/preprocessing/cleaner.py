2ae18cb8e2ee1827bd153240a6c13286272e2fd7
Beat Horat <horat@ifi.uzh.ch>
2017-12-01 17:57:15 +0100

    Revision @param descriptions

--- mtrain/preprocessing/cleaner.py
+++ mtrain/preprocessing/cleaner.py
@@ -7,11 +7,14 @@ Replaces or cleans characters with special meaning in Moses or Nematus.
 '''
 
 def clean(segment):
+    '''
+    @param segment segment to be cleaned
+    '''
     return segment # no additional cleaning by now
 
 def escape_special_chars(segment):
     '''
-    Escapes characters with special meaning in Moses.
+    Escapes characters in @param segment with special meaning in Moses.
     '''
     for char, replacement in MOSES_SPECIAL_CHARS.items():
         segment = segment.replace(char, replacement)
@@ -19,7 +22,7 @@ def escape_special_chars(segment):
 
 def deescape_special_chars(segment):
     '''
-    Deespaces characters with special meaning in Moses.
+    Deespaces characters in @param segment with special meaning in Moses.
     '''
     for char in reversed(list(MOSES_SPECIAL_CHARS)): # ugly, but needed for python 3.4.2
         segment = segment.replace(MOSES_SPECIAL_CHARS[char], char)
@@ -30,8 +33,8 @@ def normalize_romanian(segment):
     From script https://github.com/rsennrich/wmt16-scripts/blob/master/preprocess/normalise-romanian.py
     Author: Barry Haddow, Distributed under MIT license
     "Normalise Romanian s-comma and t-comma"
+    in @param segment for backend nematus
     '''
-
     # See http://www.fileformat.info/info/unicode/char:
     # tested ok, e.g. 'Iniţiaţi' with cedilla replaced by 'Inițiați' with comma below
     segment = segment.replace("\u015e", "\u0218").replace("\u015f", "\u0219")
@@ -43,6 +46,7 @@ def remove_ro_diacritics(segment):
     From script https://github.com/rsennrich/wmt16-scripts/blob/master/preprocess/remove-diacritics.py
     Author: Barry Haddow, Distributed under MIT license
     "Remove Romanian diacritics. Assumes s-comma and t-comma are normalised"
+    in @param segment for backend nematus
     '''
     # See http://www.fileformat.info/info/unicode/char:
     # tested ok, e.g. 'Adică' replaced by 'Adica'
