e24e7832679c1d8902610cd4a414bed65adf8df2
Beat Horat <horat@ifi.uzh.ch>
2017-12-14 16:41:35 +0100

    Test BPE, needs debugging

--- mtrain/test/test_bpe.py
+++ mtrain/test/test_bpe.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python3
 
+import os
+import random
+
 from mtrain.test.test_case_with_cleanup import TestCaseWithCleanup
 
-from mtrain.preprocessing import bpe
+from mtrain.preprocessing.bpe import TranslationDecoder
+from mtrain.training import TrainingNematus
+from mtrain.constants import *
+
+
+class TestEncoder(TestCaseWithCleanup):
+    '''
+    '''
+    def get_random_basename(self):
+        return str(self._basedir_test_cases + os.sep + str(random.randint(0, 9999999)))
+
+    def test_learn_bpe_model(self):
+        random_basedir_name = self.get_random_basename()
+        os.mkdir(random_basedir_name)
+
+        '''
+        t = TrainingNematus(random_basedir_name, "en", "fr", TRUECASING, 50, None)
+        t.preprocess()
+        t.train_truecaser()
+
+        # _create_random_parallel_corpus_files !!!!!
+
+
+        #t.truecase()
+        #e = TranslationDecoder(1000)
+        # e.learn_bpe_model()
+        '''
+
 
 '''
-class TestEncoder(?):
-    def test_learn_bpe_model(?):
-        pass
     def test_apply_bpe_model(?):
         pass
     def test_build_bpe_dictionary(?):
@@ -24,13 +51,14 @@ class TestTranslationDecoder(TestCaseWithCleanup):
     Decoding must replace strings "@@ " with empty string "".
     '''
     test_cases = {
-        "this is an ex@@ ample sent@@ ence .": "this is an example sentence ."
-        "esta es una oracion de ej@@ emplo .": "esta es una oracion de ejemplo ."
-        "это приме@@ рное пр@@ едложение .": "это примерное предложение ."
-        "dies ist ein Bei@@ spiel@@ satz .": "dies ist ein Beispielsatz ."
+        "this is an ex@@ ample sent@@ ence .": "this is an example sentence .",
+        "esta es una oracion de ej@@ emplo .": "esta es una oracion de ejemplo .",
+        "это приме@@ рное пр@@ едложение .": "это примерное предложение .",
+        "dies ist ein Bei@@ spiel@@ satz .": "dies ist ein Beispielsatz .",
         "acesta este un ex@@ emplu de propo@@ zitie .": "acesta este un exemplu de propozitie ."
     }
 
     def test_decode(self):
-        for example, decoded in self.test_cases.items():
-            self.assertEqual(bpe.decode(source), decoded)
+        decoder = TranslationDecoder()
+        for example_segment, decoded_segment in self.test_cases.items():
+            self.assertEqual(decoder.decode(example_segment), decoded_segment)
